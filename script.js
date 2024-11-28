25ffd256df3f76075db7a8d337c63ae02a7d1291
parsaM110 <pmomeni80@gmail.com>
2024-11-28 19:50:38 +0330

    make a code to console.log in active tab

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-async function fetchData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments');
-  const data = await response.json();
-  console.log(data);
-//   document.getElementById('concerts').innerHTML = JSON.stringify(data);
-// document.getElementById('concerts').innerHTML = data.map(item => item.email).join('<br>');
-document.getElementById('concerts').innerHTML = data.map(item => `<li>${item.email}</li>`).join('');
-}
-fetchData();
+document.getElementById("enable-loop").addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: () => {
+          console.log("Logged in active tab!");
+        }
+      });
+    });
+  });
