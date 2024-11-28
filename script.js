2097b5fe1214bf46ef02eeb36aa08d8174de0c13
parsaM110 <pmomeni80@gmail.com>
2024-11-28 19:55:37 +0330

    adding some effect for enabling and desable | there is no memory though

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+let isActive = false;
+
 document.getElementById("enable-loop").addEventListener("click", () => {
+
+  const button = document.getElementById("enable-loop");
+  
+  isActive = !isActive;
+  
+  if (isActive) {
+    button.style.backgroundColor = "red";
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: () => {
-          console.log("Logged in active tab!");
-        }
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          function: () => {
+            console.log("Logged in active tab!");
+          }
+        });
       });
-    });
-  });
+  } else {
+    button.style.backgroundColor = "gray";
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          function: () => {
+            console.log("Logged in unactive tab!");
+          }
+        });
+      });
+  }
+});
+
+
+
+    
