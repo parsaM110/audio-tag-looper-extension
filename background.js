651e59c71e383ed4d1bc2ef660a558124f423118
parsaM110 <pmomeni80@gmail.com>
2024-12-11 17:19:36 +0330

    moving the working code for icon to old code | now it is working but some bug is expected

--- background.js
+++ background.js
@@ -4,19 +4,30 @@ chrome.runtime.onInstalled.addListener(() => {
   });
   
   // Listener for toolbar icon click
-  chrome.action.onClicked.addListener((tab) => { 
-    chrome.action.setIcon({ 
-      path: {
-        '16': '/data/icons'  + '/disabled' + '/16.png',
-        '32': '/data/icons'  + '/disabled' + '/32.png',
-        '48': '/data/icons' +  '/disabled' + '/48.png'
-      }
-  });
-  });
+  chrome.action.onClicked.addListener((tab) => {
+    chrome.storage.sync.get('loopState', (data) => {
+      const newState = !data.loopState;
 
-  // chrome.action.onClicked.addListener((tab) => {
-  //   chrome.tabs.create({url: "https://www.youtube.com"});
-  // });
+      chrome.action.setIcon({ 
+        path: {
+          '16': '/data/icons'  + '/disabled' + '/16.png',
+          '32': '/data/icons'  + '/disabled' + '/32.png',
+          '48': '/data/icons' +  '/disabled' + '/48.png'
+        }
+      });
+  
+      // Update the loop state in storage
+      chrome.storage.sync.set({ loopState: newState }, () => {
+        
+        // Inject script to toggle the loop attribute on all audio tags
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          func: toggleAudioLoop,
+          args: [newState],
+        }).catch((error) => console.error('Script execution error:', error));
+      });
+    });
+  });
   
   // Function to toggle loop attribute on audio elements
   function toggleAudioLoop(state) {
